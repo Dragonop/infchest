c861d439437112d424a14d2831fb4e03a94b7bed
Joaquin Villalba <Dragonop@users.noreply.github.com>
2020-11-19 20:04:07 -0300

    Added 'give' priv check, listrings, alias
    Added 'give' priv check (allow_metadata_inventory_put, take, and move). 
    'Src' can now only be modified if player has the 'give' command. 
    Improved listrings as to behave in a sensible way.
    Added alias "infchest" for "infchest:infchest".

--- init.lua
+++ init.lua
@@ -8,15 +8,61 @@ local infchest_formspec =
 	"list[current_name;dst;4,0.5;3,3;]"..
 	"list[current_player;main;0,4.25;8,1;]"..
 	"list[current_player;main;0,5.5;8,3;8]"..
-	"listring[current_name;dst]"..
-	"listring[current_player;main]"..
 	"listring[current_name;src]"..
 	"listring[current_player;main]"..
+	"listring[current_name;dst]"..
 	"listring[current_player;main]"..
 	default.get_hotbar_bg(0, 4.25)
 
--- Node definitions
+-- Node callback functions
+local function can_dig(pos, player) -- Do not allow node to be dug if 'src' has item
+	local meta = minetest.get_meta(pos);
+	local inv = meta:get_inventory()
+	return inv:is_empty("src")
+end
 
+local function allow_metadata_inventory_put(pos, listname, index, stack, player)
+	if minetest.is_protected(pos, player:get_player_name()) then
+		return 0
+	end
+	local meta = minetest.get_meta(pos)
+	local inv = meta:get_inventory()
+	
+	if listname == "dst" then
+		return stack:get_count()
+	elseif listname == "src" and minetest.check_player_privs(player:get_player_name(),{give=true}) and inv:is_empty("src") then
+		return 1 -- Only allow one item to be selected
+	else
+		return 0
+	end
+end
+
+local function allow_metadata_inventory_move(pos, from_list, from_index, to_list, to_index, count, player)
+	local meta = minetest.get_meta(pos)
+	local inv = meta:get_inventory()
+	local stack = inv:get_stack(from_list, from_index)
+	
+	if from_list == "dst" and to_list == "dst" then
+		return allow_metadata_inventory_put(pos, to_list, to_index, stack, player)
+	elseif minetest.check_player_privs(player:get_player_name(),{give=true}) then
+		return allow_metadata_inventory_put(pos, to_list, to_index, stack, player)
+	else 
+		return 0
+	end
+end
+
+local function allow_metadata_inventory_take(pos, listname, index, stack, player)
+	-- if minetest.is_protected(pos, player:get_player_name()) then return 0 end
+	if listname == "dst" then
+		return stack:get_count()
+	elseif listname == "src" and minetest.check_player_privs(player:get_player_name(),{give=true}) then
+		return stack:get_count()
+	else 
+		return 0
+	end
+end
+
+-- Node definitions
 minetest.register_node("infchest:infchest", {
 	description = "Infinite Chest",
 	tiles = {
@@ -32,15 +78,15 @@ minetest.register_node("infchest:infchest", {
 	sounds = default.node_sound_wood_defaults(),
 	
 	can_dig = can_dig,
+	allow_metadata_inventory_put = allow_metadata_inventory_put,
+	allow_metadata_inventory_move = allow_metadata_inventory_move,
+	allow_metadata_inventory_take = allow_metadata_inventory_take
 })
 
-local function can_dig(pos, player)
-	local meta = minetest.get_meta(pos);
-	local inv = meta:get_inventory()
-	return inv:is_empty("src")
-end
+-- Alias
+minetest.register_alias("infchest", "infchest:infchest")
 
--- ABM
+-- ABM -- (optimization needed)
 minetest.register_abm({
     label = "infchest generating",
 	nodenames = {"infchest:infchest"},
@@ -52,10 +98,7 @@ minetest.register_abm({
 		
 		-- Inizialize inventory
 		local inv = meta:get_inventory()
-		for listname, size in pairs({
-				src = 1,
-				dst = 9
-		}) do
+		for listname, size in pairs({src = 1, dst = 9}) do
 			if inv:get_size(listname) ~= size then
 				inv:set_size(listname, size)
 			end
